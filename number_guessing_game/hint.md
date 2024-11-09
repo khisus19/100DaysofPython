ed6299493adab71939727858cfea849f271fc8ab
khisus19 <khisus19@gmail.com>
2024-11-09 15:26:57 -0600

    functionality achieve

--- number_guessing_game/hint.md
+++ number_guessing_game/hint.md
@@ -9,3 +9,21 @@ The goal is to build a guess the number game
 ## ASCII Art
 
 [Link Text](https://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20)
+
+
+## TODO
+
+1. Choose dificulty
+
+2. Comp choose a random number
+
+3. Program ask the user a number
+
+4. Program checks the number guessed
+
+4.1 If it is the correct the programs shows victory
+
+4.2 If is low or high programs informs the player and substract a life
+
+5. If the players lives runs down Game Over
+
--- number_guessing_game/main.py
+++ number_guessing_game/main.py
@@ -1,13 +1,51 @@
+import random
+
 from art import logo
 
-# print(logo)
+print(logo)
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
 
 player_lives = 0
 
-difficulty = input("Choose a difficulty. Type 'e' fro easy mode or 'h' for hard mode: ")
+keep_playing = "y"
 
-if difficulty == "h":
-	player_lives = 5
-else:
-	player_lives = 10
+while keep_playing == "y":
 
+	difficulty = input("Choose a difficulty. Type 'e' for easy mode or 'h' for hard mode: ")
+
+	# Asssign the lives according to difficulty
+	if difficulty == "h":
+		player_lives = 5
+	else:
+		player_lives = 10
+	print(f"You have {player_lives} attempts remaining to guess the number.")
+
+	# Computer chooses a random number
+	comp_number = random.randint(1, 100)
+	print(comp_number)
+
+	# Player guess a number
+	player_guess = int(input("Choose a number: "))
+
+	while player_guess != comp_number and player_lives > 0:
+		if player_guess > comp_number:
+			print("Too high")
+			player_lives -= 1
+		else:
+			print("Too low")
+			player_lives -= 1
+		
+		if player_lives == 0:
+			break
+
+		print("Guess again")
+		print(f"You have {player_lives} attempts remaining to guess the number.")
+		player_guess = int(input("Choose a number: "))
+
+	if player_guess == comp_number:
+		print("You have guessed the number")
+	else:
+		print("You've run out of guesses.")
+		print(f"The correct number was: {comp_number}")
+	keep_playing = input("Do you want to keep playing. Type 'y' if so: ")
