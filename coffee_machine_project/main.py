6b8e9fc6f1959f51c617ea08a72be78c3f82a1d6
khisus19 <khisus19@gmail.com>
2024-11-13 12:10:25 -0600

    Create to check if there is enough resources in inventory to continue with the sale

--- coffee_machine_project/main.py
+++ coffee_machine_project/main.py
@@ -37,13 +37,29 @@ coin_values = {
     "penny": 0.01,
 }
 
+# Function to calculate money entered by usser
 def calc_input_money(coin_1, coin_2, coin_3, coin_4):
+    '''Takes the quantity of each coin and returns the total value'''
     return (coin_1 * 0.25) + (coin_2 * 0.1) + (coin_3 * 0.05) + (coin_4 * 0.01)
 
 
-# funcion que calcule el dinero que ingresó
-# funcion que verifique si el dinero ingresado es suficiente
-# función que actualizar el inventario (restar recursos y guardar el dinero)
+# Function to check if the user has sufficient money to buy the chosen drink
+def check_for_sufficient_money(user_money, chosen_drink):
+    if user_money < MENU[chosen_drink]["cost"]:
+        print("You don't have enough money")
+    
+# function to check if there is enough resources in inventory
+def check_resources(chosen_drink, resource_left):
+    if resource_left["water"] < MENU[chosen_drink]["ingredients"]["water"]:
+        return "Sorry there is not enough water."
+    elif resource_left["milk"] < MENU[chosen_drink]["ingredients"]["milk"]:
+        return "Sorry there is not enough milk."
+    elif resource_left["coffe"] < MENU[chosen_drink]["ingredients"]["coffe"]:
+        return "Sorry there is not enough coffee."
+
+# Function to update the inventory if the purchase is succesful (substract resources and save the money)
+
+
 # funcion que muestre el reporte de los recursos restantes y el dinero obtenido
 
 # funcion de compra
