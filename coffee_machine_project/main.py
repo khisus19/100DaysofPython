4e258693e4bff6a7ba37ecf30fd022000718d167
khisus19 <khisus19@gmail.com>
2024-11-13 11:47:07 -0600

    Coffee machine start

---
+++ coffee_machine_project/main.py
@@ -0,0 +1,56 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+coin_values = {
+    "quarter": 0.25,
+    "dime": 0.1,
+    "nickel": 0.05,
+    "penny": 0.01,
+}
+
+
+
+
+# funcion que calcule el dinero que ingresó
+# funcion que verifique si el dinero ingresado es suficiente
+# función que actualizar el inventario (restar recursos y guardar el dinero)
+# funcion que muestre el reporte de los recursos restantes y el dinero obtenido
+
+# funcion de compra
+''' Debe preguntar qué bebida quiere
+    Debe preguntar el dinero
+    Debe calcular cuánto dinero es
+    Debe chequear que sea suficiente
+    Si no es suficiente informar y devolver el dinero
+    Si es suficiente debe actualizar el inventario
+    Mostrar el output (cafe y cambio)
+'''
