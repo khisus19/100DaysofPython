f40760a0cd3cdd7ae20f3bc39adad6858e46921a
khisus19 <khisus19@gmail.com>
2024-11-14 20:30:07 -0600

    Create check for enough resource function

--- coffee_machine_project/main.py
+++ coffee_machine_project/main.py
@@ -2,6 +2,7 @@ MENU = {
     "espresso": {
         "ingredients": {
             "water": 50,
+            "milk": 0,
             "coffee": 18,
         },
         "cost": 1.5,
@@ -46,15 +47,37 @@ def calc_input_money(coin_1, coin_2, coin_3, coin_4):
 # Function to check if the user has sufficient money to buy the chosen drink
 def check_for_sufficient_money(user_money, chosen_drink):
     if user_money < MENU[chosen_drink]["cost"]:
-        print("You don't have enough money")
+        # print("You don't have enough money")
+        return True
+    else:
+        return False
     
 # function to check if there is enough resources in inventory
+def is_sufficient_water(chosen_drink, resource_left):
+    if resource_left["water"] > MENU[chosen_drink]["ingredients"]["water"]:
+        return True
+    else:
+        return False
+
+def is_sufficient_milk(chosen_drink, resource_left):
+    if resource_left["milk"] > MENU[chosen_drink]["ingredients"]["milk"]:
+        return True
+    else:
+        return False
+    
+def is_sufficient_coffee(chosen_drink, resource_left):
+    if resource_left["coffee"] > MENU[chosen_drink]["ingredients"]["coffee"]:
+        return True
+    else:
+        return False
+
+
 def check_resources(chosen_drink, resource_left):
-    if resource_left["water"] < MENU[chosen_drink]["ingredients"]["water"]:
+    if not is_sufficient_water(chosen_drink, resource_left):
         return "Sorry there is not enough water."
-    elif resource_left["milk"] < MENU[chosen_drink]["ingredients"]["milk"]:
+    elif not is_sufficient_milk(chosen_drink, resource_left):
         return "Sorry there is not enough milk."
-    elif resource_left["coffe"] < MENU[chosen_drink]["ingredients"]["coffe"]:
+    elif not is_sufficient_coffee(chosen_drink, resource_left):
         return "Sorry there is not enough coffee."
 
 # Function to update the inventory if the purchase is succesful (substract resources and save the money)
@@ -71,3 +94,12 @@ def check_resources(chosen_drink, resource_left):
     Si es suficiente debe actualizar el inventario
     Mostrar el output (cafe y cambio)
 '''
+
+def purchase():
+    selected_drink = input("What would you like? (espresso/latte/cappuccino): ")
+    selected_drink = "latte"
+
+    if (is_sufficient_water(selected_drink, resources) or is_sufficient_milk(selected_drink, resources) or is_sufficient_coffee(selected_drink, resources)):
+        print(check_resources(selected_drink, resources))
+
+purchase()
