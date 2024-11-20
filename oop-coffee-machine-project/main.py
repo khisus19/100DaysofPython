6155ab292b87a627032d5034ac250fc1bcd43533
khisus19 <khisus19@gmail.com>
2024-11-19 22:04:36 -0600

    Create final project following the teacher

--- oop-coffee-machine-project/main.py
+++ oop-coffee-machine-project/main.py
@@ -3,37 +3,29 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 my_menu = Menu()
-my_machine = CoffeeMaker()
+my_coffee_machine = CoffeeMaker()
+my_money_machine = MoneyMachine()
+is_on = True
 
-def calculate_total_money(qu, di, ni, pe):
-	return round((qu * 0.25) + (di * 0.1) + (ni * 0.05) + (pe * 0.01), 2)
 
-user_choice = input("What would you like? (espresso/latte/cappuccino/): ")
 
-while user_choice != "off":
+while is_on:
+	options = my_menu.get_items()
 
-	if user_choice == "report":
-		my_machine.report()
+	user_choice = input(f"What would you like? ({options}): ")
 
-	user_choice = "latte"
+	if user_choice == "off":
+		is_on = False
+	elif user_choice == "report":
+		my_coffee_machine.report()
+		my_money_machine.report()
+	else:
+		drink = my_menu.find_drink(user_choice)
 
-	user_drink = my_menu.find_drink(user_choice)
+		# TODO: If resource is sufficient continue with the sale my_coffee_machine.is_resource_sufficient(user_drink)
+		if my_coffee_machine.is_resource_sufficient(drink):
+			# TODO: check if the transaction was succesfull
 
-	# TODO: If resource is sufficient continue with the sale my_machine.is_resource_sufficient(user_drink)
-	if my_machine.is_resource_sufficient(user_drink):
-		print("Please insert the coins")
-		user_quarters = int(input("How many quarters?: "))
-		user_dimes = int(input("How many dimes?: "))
-		user_nickels = int(input("How many nickels?: "))
-		user_pennies = int(input("How many pennies?: "))
-		user_money = calculate_total_money(user_quarters, user_dimes, user_nickels, user_pennies)
+			if my_money_machine.make_payment(drink.cost):
 
-		print(user_money)
-		
-
-	print()
-
-	# print(my_machine.is_resource_sufficient(user_drink))
-
-	
-	user_choice = input("What would you like? (espresso/latte/cappuccino/): ")
+				my_coffee_machine.make_coffee(drink)
