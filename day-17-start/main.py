83840d7938683495ce0f882df26271a6725c9733
khisus19 <khisus19@gmail.com>
2024-11-19 22:52:12 -0600

    Practicing initializing a class instance and adding attributes

--- day-17-start/main.py
+++ day-17-start/main.py
@@ -1,6 +1,13 @@
 class User:
-	pass
+	def __init__(self, user_id, username):
+		self.id = user_id
+		self.username = username
+		self.followers = 0
 
-user_1 = User()
+user_1 = User("001", "angela")
+
+print(user_1.followers)
+
+user_2 = User("002", "jack")
 
 
