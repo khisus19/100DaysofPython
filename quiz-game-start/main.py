466a0592128788d4cce20d039b8b3ade373db66b
khisus19 <khisus19@gmail.com>
2024-11-20 23:57:09 -0600

    Initialize QuizBrain

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -7,3 +8,5 @@ for each_question in question_data:
 	my_q = Question(each_question["text"], each_question["answer"])
 	question_bank.append(my_q)
 
+quiz = QuizBrain(question_bank)
+quiz.next_question()
