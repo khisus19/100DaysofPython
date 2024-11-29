db5b3f0298c061daf0d223f48a8ecc75faa536ab
khisus19 <khisus19@gmail.com>
2024-11-28 21:02:47 -0600

    Fourth challenge complete

--- day-18-start/main.py
+++ day-18-start/main.py
@@ -5,9 +5,11 @@ from turtle import Turtle, Screen
 tim = Turtle()
 tim.shape("turtle")
 
-colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown", "aquamarine", "PaleGreen", "cyan", "magenta", "lime", "gold", "gray"]
+colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown", "cyan", "magenta", "lime", "gold", "gray", "violet", "indigo", "turquoise", "maroon", "navy", "teal", "salmon", "coral", "khaki", "orchid", "plum", "tan"]
 
-# First challenge: "Make a square"
+###--------- First challenge: --------###
+#           Make a square
+
 # tim.color("red")
 # tim.forward(100)
 # tim.left(90)
@@ -17,28 +19,53 @@ colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown",
 # tim.left(90)
 # tim.forward(100)
 
-# Second chalenge: Draw a dashed line
+
+###--------- Second chalenge: --------###
+#           Draw a dashed line 
+
 # for i in range(20):
 #     tim.forward(10)
 #     tim.penup()
 #     tim.forward(10)
 #     tim.pendown()
 
-# Third Challenge: Draw a triangle, square, pentagon, hexagon, heptagon, octagon, nonagon and decagon
-def draw_shape(sides):
-    for _ in range(sides):
-        tim.forward(100)
-        tim.right(360/sides)
+
+###--------- Third Challenge: --------###
+#            Draw a triangle, square, pentagon, hexagon, heptagon, octagon, nonagon and decagon
+
+# def draw_shape(sides):
+#     for _ in range(sides):
+#         tim.forward(100)
+#         tim.right(360/sides)
+
+# def change_pen_color():
+#     color = random.choice(colors)
+#     tim.pencolor(color)
+#     colors.remove(color)
+
+# for number_of_sides in range(3,11):
+#     change_pen_color()
+#     draw_shape(number_of_sides)
+
+
+###--------- Fourth challenge: --------###
+#            Random walk
+
+tim.speed(9)
+tim.pensize(10)
+
+directions = (0, 90, 180, 270)
 
 def change_pen_color():
-    color = random.choice(colors)
-    tim.pencolor(color)
-    colors.remove(color)
+    tim.pencolor(random.choice(colors))
 
-for number_of_sides in range(3,11):
+def choose_direction():
+    tim.setheading(random.choice(directions))
+
+for _ in range(130):
+    choose_direction()
+    tim.forward(30)
     change_pen_color()
-    draw_shape(number_of_sides)
-
 
 
 screen = Screen()
