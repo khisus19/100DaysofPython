1a438e4304ba697ecf4574c3d8413b45e5b3cbe6
khisus19 <khisus19@gmail.com>
2024-11-28 18:54:58 -0600

    First challenge complete

--- day-18-start/main.py
+++ day-18-start/main.py
@@ -1,11 +1,17 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
+tim = Turtle()
 
+# First challenge: "Make a square"
+# tim.shape("turtle")
+# tim.color("red")
+# tim.forward(100)
+# tim.left(90)
+# tim.forward(100)
+# tim.left(90)
+# tim.forward(100)
+# tim.left(90)
+# tim.forward(100)
 
 
 
