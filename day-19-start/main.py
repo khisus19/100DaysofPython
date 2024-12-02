d0dbab9975758ad51c459f1dfcc9c843794aaa14
khisus19 <khisus19@gmail.com>
2024-12-02 12:05:32 -0600

    Set screen size and user input

--- day-19-start/main.py
+++ day-19-start/main.py
@@ -3,33 +3,12 @@ import turtle as t
 tim = t.Turtle()
 
 screen = t.Screen()
-
-def move_forward():
-    tim.forward(10)
-
-def move_backward():
-    tim.backward(10)
-
-def turn_right():
-    tim.right(10)
-
-def turn_left():
-    tim.left(10)
-
-def clear():
-    tim.clear()
-    tim.penup()
-    tim.home()
-    tim.pendown()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Enter a color")
 
 
 
-screen.listen()
-screen.onkey(move_forward, "w")
-screen.onkey(move_backward, "s")
-screen.onkey(turn_right, "d")
-screen.onkey(turn_left, "a")
-screen.onkey(clear, "c")
+
 
 
 screen.exitonclick()
