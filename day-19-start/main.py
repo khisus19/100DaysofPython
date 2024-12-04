2d887342d1461c6ae3eb9f56d6e073f4ce5803b2
khisus19 <khisus19@gmail.com>
2024-12-04 11:07:36 -0600

    Set a for loop to create each turtle

--- day-19-start/main.py
+++ day-19-start/main.py
@@ -6,40 +6,26 @@ screen = t.Screen()
 screen.setup(width=500, height=400)
 # user_bet = screen.textinput(title="Make your bet", prompt="Enter a color")
 
-purple_turtle = t.Turtle()
-purple_turtle.shape("turtle")
-purple_turtle.setposition(-220, 75)
-purple_turtle.fillcolor("purple")
+#TODO: Maybe use a for loop to give the turtles their shape
+colors_list = ["purple", "blue", "green", "yellow", "orange", "red"]
+
+positions = [(-220, -75), (-220, -45), (-220, -15), (-220, 15), (-220, 45), (-220, 75)]
+
+turtle_list = []
+
+for index, color in enumerate(colors_list):
+    current_turtle = t.Turtle("turtle")
+    current_turtle.penup()
+    current_turtle.fillcolor(color)
+    current_turtle.setposition(positions[index])
+    print(current_turtle.position())
+    turtle_list.append(current_turtle)
 
 
-blue_turtle = t.Turtle()
-blue_turtle.shape("turtle")
-blue_turtle.setposition(-220, 45)
-blue_turtle.fillcolor("blue")
 
-green_turtle = t.Turtle()
-green_turtle.shape("turtle")
-green_turtle.setposition(-220,15)
-green_turtle.fillcolor("green")
-
-yellow_turtle = t.Turtle()
-yellow_turtle.shape("turtle")
-yellow_turtle.setposition(-220, -15)
-yellow_turtle.fillcolor("yellow")
-
-orange_turtle = t.Turtle()
-orange_turtle.shape("turtle")
-orange_turtle.setposition(-220, -45)
-orange_turtle.fillcolor("orange")
-
-red_turtle = t.Turtle()
-red_turtle.shape("turtle")
-red_turtle.setposition(-220, -75)
-red_turtle.fillcolor("red")
 
 #TODO: Change the order on which the turtles set in the starting position
 #TODO: Make the animation slower
-#TODO: Maybe use a for loop to give the turtles their shape
 
 
 screen.exitonclick()
