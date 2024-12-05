61d606f8609992dd7d508582d43d523310c27d61
khisus19 <khisus19@gmail.com>
2024-12-05 16:35:47 -0600

    Create directional control methods

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -7,6 +7,7 @@ class Snake:
     def __init__(self):
         self.segments_list = []
         self.create_snake()
+        self.head = self.segments_list[0]
         
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -22,4 +23,16 @@ class Snake:
             new_y = self.segments_list[seg_index - 1].ycor()
             self.segments_list[seg_index].goto(new_x, new_y)
 
-        self.segments_list[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.head.setheading(90)
+
+    def down(self):
+        self.head.setheading(270)
+
+    def left(self):
+        self.head.setheading(180)
+
+    def right(self):
+        self.head.setheading(0)
