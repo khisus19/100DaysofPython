10ad0925727164909c561b23ba4c99a3968f23e5
khisus19 <khisus19@gmail.com>
2024-12-05 15:08:45 -0600

    Create the instances for each segment

--- snake_game/main.py
+++ snake_game/main.py
@@ -5,6 +5,16 @@ screen.bgcolor("black")
 screen.setup(600, 600)
 screen.title("Vibora")
 
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+segments_list = []
+
+for position in starting_positions:
+    new_segment = t.Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments_list.append(new_segment)
 
 
 
