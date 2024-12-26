b87e0665730c810cdd84a23ffd5bb451e49f46b5
khisus19 <khisus19@gmail.com>
2024-12-25 22:54:33 -0600

    Calculate Monday's temperature in Fahrenheit and print it

--- day-25-start/main.py
+++ day-25-start/main.py
@@ -25,6 +25,16 @@ temp_list = data["temp"].to_list()
 # final_sum = sum(temp_list)
 # average_temp = final_sum / len(temp_list)
 
-average_temp = data["temp"].mean()
+# average_temp = data["temp"].mean()
 
-print(average_temp)
+# maximum_temp = data["temp"].max()
+
+# print(maximum_temp)
+
+# print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+
+monday_far_temp = (monday.temp[0] * 9/5) + 32
+
+print(monday_far_temp)
