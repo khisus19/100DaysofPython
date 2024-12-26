ce0cd5c55b3768cca7ac32909f44396e47f8d2de
khisus19 <khisus19@gmail.com>
2024-12-25 23:00:10 -0600

    Refactor code to create a DataFrame from a dictionary and print it

--- day-25-start/main.py
+++ day-25-start/main.py
@@ -16,25 +16,34 @@ import pandas as pd
 
 data = pd.read_csv('./weather_data.csv')
 
-data_dict = data.to_dict()
 
-temp_list = data["temp"].to_list()
-
+# # Convert a DataFrame to a Dictionary
+# data_dict = data.to_dict()
 # print(data_dict)
+
+# # Convert a row to a list
+# temp_list = data["temp"].to_list()
 
 # final_sum = sum(temp_list)
 # average_temp = final_sum / len(temp_list)
 
 # average_temp = data["temp"].mean()
-
 # maximum_temp = data["temp"].max()
 
 # print(maximum_temp)
-
 # print(data[data.temp == data.temp.max()])
 
-monday = data[data.day == "Monday"]
+# monday = data[data.day == "Monday"]
+# monday_far_temp = (monday.temp[0] * 9/5) + 32
+# print(monday_far_temp)
 
-monday_far_temp = (monday.temp[0] * 9/5) + 32
 
-print(monday_far_temp)
+# Create a DataFrame or a table from a dictionary
+
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data = pd.DataFrame(data_dict)
+print(data)
