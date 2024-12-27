b22f7f6e4ccd5bad8dbfedd868963719c05636dd
khisus19 <khisus19@gmail.com>
2024-12-27 12:40:23 -0600

    Add functionality to track missed states and export to CSV

--- day-25-us-states-game-start/main.py
+++ day-25-us-states-game-start/main.py
@@ -13,25 +13,35 @@ t.shape(image)
 
 # Read the data
 data = pd.read_csv("./50_states.csv")
-states_list = list(data.state)
+states_list = data.state.to_list()
 
-is_game_on = True
-correct_answers_list = []
+guessed_list = []
 score = 0
 
-while is_game_on:
+while len(guessed_list) < 50:
 
     player_input = screen.textinput(f"{score}/50 States Correct", "Write a state name").title()
-    print(player_input)
 
-    if player_input in states_list and player_input not in correct_answers_list:
+    if player_input == "Exit":
+        missing_states = []
+        for state in states_list:
+            if state not in guessed_list:
+                missing_states.append(state)
+        print(missing_states)
+        missing_states_df = pd.DataFrame(missing_states, columns=["state"])
+        missing_states_df.to_csv("states_to_learn.csv")
+        break
+
+    if player_input in states_list and player_input not in guessed_list:
         player_input_row = data[data.state == player_input]
         player_input_x_coor = int(player_input_row["x"].iloc[0])
         player_input_y_coor = int(player_input_row["y"].iloc[0])
-        correct_answers_list.append(player_input)
+        guessed_list.append(player_input)
 
         score += 1
         state = State(player_input, player_input_x_coor, player_input_y_coor)
     
+
+# states_to_learn.csv
 
 t.mainloop()
