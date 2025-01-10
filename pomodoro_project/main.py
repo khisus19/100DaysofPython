6746cd08be2c0bce5c34ffb314266b540ed2e317
khisus19 <khisus19@gmail.com>
2025-01-10 11:21:48 -0600

    Add timer label and buttons to Pomodoro timer UI

--- pomodoro_project/main.py
+++ pomodoro_project/main.py
@@ -9,6 +9,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+CHECKMARK = "✔"
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -25,11 +26,19 @@ canvas = Canvas(width=210, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="./tomato.png")
 canvas.create_image(103, 112, image=tomato_img)
 canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(row=1, column=1)
 
 
+timer_label = Label(window, text="Timer", font=(FONT_NAME, 40, "bold"), bg=YELLOW, fg=GREEN)
+timer_label.grid(row=0, column=1)
 
+check_marks_label = Label(window, text=f"{CHECKMARK}", font=(FONT_NAME, 14), fg=GREEN, bg=YELLOW)
+check_marks_label.grid(row=3, column=1)
 
+start_button = Button(text="Start", command=None, font=("Helvetica", 14, "normal"), highlightthickness=0, bg=YELLOW)
+start_button.grid(row=2, column=0)
 
+reset_button = Button(text="Reset", command=None, font=("Helvetica", 14, "normal"), highlightthickness=0, bg=YELLOW)
+reset_button.grid(row=2, column=2)
 
 window.mainloop()
