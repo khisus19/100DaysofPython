3226830c7e49a58bdc9b616750049753ac023b68
khisus19 <khisus19@gmail.com>
2025-01-09 18:18:17 -0600

    Practice with Tkinter widgets

--- day-27/main.py
+++ day-27/main.py
@@ -4,11 +4,28 @@ window = tk.Tk()
 window.title("My first GUI Program")
 window.minsize( 500, 300)
 
+
+def button_clicked():
+    input_text = input.get()
+    my_label.config(text=input_text)
+
 # Label
 my_label = tk.Label(window, text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
 
 
+# Button
+button = tk.Button(text="Click Me", command=button_clicked)
+button.grid(column=1, row=1)
 
+# Button 2
+new_button = tk.Button(text="Click me too")
+new_button.grid(column=2, row=0)
+
+# Entry
+
+input = tk.Entry(width=10)
+input.grid(column=3, row=2)
 
 window.mainloop()
