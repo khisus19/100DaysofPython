0f8533f6f65dfaba96120e8ae9c542c9566ef4c0
khisus19 <khisus19@gmail.com>
2025-01-11 12:33:55 -0600

    Refactor Pomodoro timer logic to include reset functionality and adjust timer durations

--- pomodoro_project/main.py
+++ pomodoro_project/main.py
@@ -10,12 +10,35 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 CHECKMARK = "✔"
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    check_marks_label.config(text="")
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(WORK_MIN * 60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps == 8:
+        count_down(long_break_sec)
+        timer_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Working", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
@@ -25,8 +48,15 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_minutes}:{count_seconds:02d}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
-
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        work_sessions = int(reps/2)
+        for _ in range(work_sessions):
+            mark += CHECKMARK
+        check_marks_label.config(text=mark)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -42,16 +72,16 @@ timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT
 canvas.grid(row=1, column=1)
 
 
-timer_label = Label(window, text="Timer", font=(FONT_NAME, 40, "bold"), bg=YELLOW, fg=GREEN)
+timer_label = Label(window, text="Timer", font=(FONT_NAME, 35, "bold"), bg=YELLOW, fg=GREEN)
 timer_label.grid(row=0, column=1)
 
-check_marks_label = Label(window, text=f"{CHECKMARK}", font=(FONT_NAME, 14), fg=GREEN, bg=YELLOW)
+check_marks_label = Label(window, text="", font=(FONT_NAME, 14), fg=GREEN, bg=YELLOW)
 check_marks_label.grid(row=3, column=1)
 
 start_button = Button(text="Start", command=start_timer, font=("Helvetica", 14, "normal"), highlightthickness=0, bg="white")
 start_button.grid(row=2, column=0)
 
-reset_button = Button(text="Reset", command=None, font=("Helvetica", 14, "normal"), highlightthickness=0, bg="white")
+reset_button = Button(text="Reset", command=reset_timer, font=("Helvetica", 14, "normal"), highlightthickness=0, bg="white")
 reset_button.grid(row=2, column=2)
 
 window.mainloop()
