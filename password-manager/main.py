b712e3a5786d860abfc7cabad790846840781478
khisus19 <khisus19@gmail.com>
2025-01-12 20:59:04 -0600

    Add UI setup for Password Manager with logo display

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,5 +1,19 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+screen = Tk()
+screen.title("Password Manager")
+screen.config(bg="white", padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
+logo_img = PhotoImage(file="./logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=0)
+
+
+screen.mainloop()
