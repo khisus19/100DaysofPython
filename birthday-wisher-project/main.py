961accfa50d557e930919a5800d5810a9340564f
khisus19 <khisus19@gmail.com>
2025-01-31 10:25:03 -0600

    Update email address for birthday reminder and refactor main script for clarity

--- birthday-wisher-project/main.py
+++ birthday-wisher-project/main.py
@@ -4,38 +4,37 @@ import pandas as pd
 my_gmail = "pruebapython5@gmail.com"
 gmail_server = "smtp.gmail.com"
 gmail_app_pwd = "llea bnmy oldl ysgl"
-my_yahoo = "pythonrecipient@yahoo.com"
 yahoo_server = "smpt.mail.yahoo.com"
 
-
-##################### Extra Hard Starting Project ######################
-
-# 1. Update the birthdays.csv
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
+## Extract todays date
 today = datetime.datetime.now()
-year_today = today.year
 month_today = today.month
 day_today = today.day
 
+## Read the csv file and convert it to a dictionary to work with
 df = pd.read_csv("./birthdays.csv")
 birthdays_dict = df.to_dict("records")
 
+
+## Loop through all the birthdays to check if today is someones birthday
 for date in birthdays_dict:
     if date["month"] == month_today and date["day"] == day_today:
-        print(f"El cumpleaño de {date["name"]} es este mes. Cumple el {date["day"]}")
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
+## If is true we pick a random letter and replace the birthday person's name
         with open(f"./letter_templates/letter_{random.randint(1,3)}.txt", "r") as letter_file:
-
             letter_content = letter_file.read()
             letter_content = letter_content.replace("[NAME]", date["name"])
-            print(letter_content)
 
-# 4. Send the letter generated in step 3 to that person's email address.
+## Send the letter generated in step 3 to that person's email address.
 
+        with smtplib.SMTP(gmail_server, port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_gmail, password=gmail_app_pwd)
+            connection.sendmail(
+                from_addr=my_gmail,
+                to_addrs=date["email"],
+                msg=f"Subject:Happy Birthday!!\n\n{letter_content}"
+            )
 
 
 
