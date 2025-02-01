f1e280b083924fe7cd53973451dbec9023f6bd84
khisus19 <khisus19@gmail.com>
2025-02-01 13:12:33 -0600

    Add QuizInterface class for GUI implementation and integrate with quiz logic

--- quizzler-app-project/ui.py
+++ quizzler-app-project/ui.py
@@ -1,2 +1,18 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+
+        label_score = Label(text="Score: 0", font=("Arial", 15, "normal"))
+        label_score.grid(row=0, column=1)
+        label_question = Label(bg="white", text="This is the question", font=("Arial", 20, "italic"))
+        label_question.grid(row=1, column=0, columnspan=2)
+
+        self.window.mainloop()
