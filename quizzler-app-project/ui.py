fde06d2d3f78deb230c76c068cc63b71e47320aa
khisus19 <khisus19@gmail.com>
2025-02-02 23:21:27 -0600

    Refactor QuizInterface to accept QuizBrain instance and update question display logic

--- quizzler-app-project/ui.py
+++ quizzler-app-project/ui.py
@@ -1,11 +1,14 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
@@ -14,14 +17,16 @@ class QuizInterface:
         self.score_label.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=250, bg="white")
-        self.question_text = self.canvas.create_text(
-            150, 
-            125, 
-            text="Some Question text", 
-            fill=THEME_COLOR,
-            font=("Arial", 20, "italic")
-        )
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        self.question_label = Label(
+            text="Some Question text", 
+            font=("Arial", 20, "italic"), 
+            fg=THEME_COLOR, 
+            bg="white",
+            wraplength=280
+        )
+        self.question_label.grid(row=1, column=0, columnspan=2)
 
         true_img = PhotoImage(file="./images/true.png")
         self.true_button = Button(image=true_img, highlightthickness=0)
@@ -31,4 +36,10 @@ class QuizInterface:
         self.false_button = Button(image=false_img, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.question_label.config(text=q_text)
