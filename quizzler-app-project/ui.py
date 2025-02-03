4cbd324cc515348122b2a1c6fa82c0a60d73ace4
khisus19 <khisus19@gmail.com>
2025-02-02 23:42:41 -0600

    Add user answer handling and score update in QuizInterface

--- quizzler-app-project/ui.py
+++ quizzler-app-project/ui.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from quiz_brain import QuizBrain
+import time
 
 THEME_COLOR = "#375362"
 
@@ -13,7 +14,7 @@ class QuizInterface:
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
 
-        self.score_label = Label(text="Score: 0", font=("Arial", 15, "normal"), bg=THEME_COLOR, fg="white")
+        self.score_label = Label(text=f"Score: {self.quiz.score}", font=("Arial", 15, "normal"), bg=THEME_COLOR, fg="white")
         self.score_label.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=250, bg="white")
@@ -21,7 +22,7 @@ class QuizInterface:
 
         self.question_label = Label(
             text="Some Question text", 
-            font=("Arial", 20, "italic"), 
+            font=("Arial", 18, "italic"), 
             fg=THEME_COLOR, 
             bg="white",
             wraplength=280
@@ -29,11 +30,11 @@ class QuizInterface:
         self.question_label.grid(row=1, column=0, columnspan=2)
 
         true_img = PhotoImage(file="./images/true.png")
-        self.true_button = Button(image=true_img, highlightthickness=0)
+        self.true_button = Button(image=true_img, highlightthickness=0, command=self.user_true)
         self.true_button.grid(row=2, column=0)
 
         false_img = PhotoImage(file="./images/false.png")
-        self.false_button = Button(image=false_img, highlightthickness=0)
+        self.false_button = Button(image=false_img, highlightthickness=0, command=self.user_false)
         self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
@@ -43,3 +44,15 @@ class QuizInterface:
     def get_next_question(self):
         q_text = self.quiz.next_question()
         self.question_label.config(text=q_text)
+
+    def user_true(self):
+        self.quiz.check_answer("True")
+        self.score_label.config(text=f"Score: {self.quiz.score}")
+        time.sleep(1)
+        self.get_next_question()
+
+    def user_false(self):
+        self.quiz.check_answer("False")
+        self.score_label.config(text=f"Score: {self.quiz.score}")
+        time.sleep(1)
+        self.get_next_question()
