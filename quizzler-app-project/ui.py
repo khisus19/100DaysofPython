897681a759482522cff19f13f2eeb215425ad1c0
khisus19 <khisus19@gmail.com>
2025-02-02 22:00:35 -0600

    Enhance QuizInterface UI with score display, question canvas, and answer buttons

--- quizzler-app-project/ui.py
+++ quizzler-app-project/ui.py
@@ -10,9 +10,25 @@ class QuizInterface:
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
 
-        label_score = Label(text="Score: 0", font=("Arial", 15, "normal"))
-        label_score.grid(row=0, column=1)
-        label_question = Label(bg="white", text="This is the question", font=("Arial", 20, "italic"))
-        label_question.grid(row=1, column=0, columnspan=2)
+        self.score_label = Label(text="Score: 0", font=("Arial", 15, "normal"), bg=THEME_COLOR, fg="white")
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150, 
+            125, 
+            text="Some Question text", 
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_img = PhotoImage(file="./images/true.png")
+        self.true_button = Button(image=true_img, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        false_img = PhotoImage(file="./images/false.png")
+        self.false_button = Button(image=false_img, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
 
         self.window.mainloop()
