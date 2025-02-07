172a81711b591f3a36941941059909ddaba4a703
khisus19 <khisus19@gmail.com>
2025-02-07 12:00:03 -0600

    Refactor Pixela user and graph creation logic; add pixel posting functionality

--- habit-tracker.project/main.py
+++ habit-tracker.project/main.py
@@ -1,12 +1,13 @@
 import requests, os
 
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
-username = "khisus19"
+USERNAME = "khisus19"
 pixela_token = os.getenv("PIXELA_TOKEN")
 
+##------------- Create a new user in Pixela -----------------##
 user_params = {
     "token": pixela_token,
-    "username": "khisus19",
+    "USERNAME": "khisus19",
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
@@ -15,7 +16,8 @@ user_params = {
 # print(response.text)
 
 
-graph_endpoint = f"{PIXELA_ENDPOINT}/{username}/graphs"
+##------------- Create a Graph in Pixela -----------------##
+graph_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
 graph_config = {
     "id": "graph1",
     "name": "Cycling Graph",
@@ -25,8 +27,23 @@ graph_config = {
 }
 
 headers = {
-    "X-USER-TOKEN": pixela_token
+    "X-USER-TOKEN": pixela_token,
 }
 
-response = requests.post(graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+
+##------------- Post a pixel -----------------##
+graphid = "graph1"
+my_graph_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{graphid}"
+
+pixel_request_body = {
+    "date": "20250206",
+    "quantity": "14",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+response = requests.post(url=my_graph_endpoint, json=pixel_request_body, headers=headers)
 print(response.text)
